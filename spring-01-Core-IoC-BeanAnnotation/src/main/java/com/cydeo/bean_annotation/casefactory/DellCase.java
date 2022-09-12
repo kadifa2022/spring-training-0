d24c516b1c78a402e409f807e4a769fdbb263a8c
Kadifa <Kudickadifa@gmail.com>
2022-09-12 05:56:40 -0500

    bean annotation

---
+++ spring-01-Core-IoC-BeanAnnotation/src/main/java/com/cydeo/bean_annotation/casefactory/DellCase.java
@@ -0,0 +1,14 @@
+package com.cydeo.bean_annotation.casefactory;
+
+public class DellCase extends Case{
+
+    public DellCase(String model, String manufacturer, String powerSupply) {
+        super(model, manufacturer, powerSupply);
+    }
+
+    @Override
+    public void pressPowerButton() {
+        System.out.println("Power button pressed");
+
+    }
+}
