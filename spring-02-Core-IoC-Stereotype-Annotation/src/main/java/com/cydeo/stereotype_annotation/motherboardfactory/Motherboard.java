edcd05a8ecad66a29bdc5e4e216a7753e0b97fde
Kadifa <Kudickadifa@gmail.com>
2022-09-12 09:30:49 -0500

    stereotype annotation

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/stereotype_annotation/motherboardfactory/Motherboard.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/stereotype_annotation/motherboardfactory/Motherboard.java
@@ -1,5 +1,11 @@
 package com.cydeo.stereotype_annotation.motherboardfactory;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public abstract class Motherboard {
     private String model;
     private String manufacturer;
