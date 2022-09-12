7fa26a496e71a01232e485e52ddcc0190aba45c2
Kadifa <Kudickadifa@gmail.com>
2022-09-12 07:08:13 -0500

    bean annotation

---
+++ spring-01-Core-IoC-BeanAnnotation/src/main/java/com/cydeo/bean_annotation/monitorfactory/AcerMonitor.java
@@ -0,0 +1,13 @@
+package com.cydeo.bean_annotation.monitorfactory;
+
+public class AcerMonitor extends Monitor{
+    public AcerMonitor(String model, String manufacturer, int size) {
+        super(model, manufacturer, size);
+    }
+
+    @Override
+    public void drawPixelAt() {
+        System.out.println("Drawing pixel with Acer");
+
+    }
+}
