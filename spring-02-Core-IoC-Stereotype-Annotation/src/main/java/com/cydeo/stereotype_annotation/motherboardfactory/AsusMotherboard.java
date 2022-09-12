dafff25b1ba6edacf82dfb73be8401883b880db7
Kadifa <Kudickadifa@gmail.com>
2022-09-12 09:30:08 -0500

    stereotype annotation

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/stereotype_annotation/motherboardfactory/AsusMotherboard.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/stereotype_annotation/motherboardfactory/AsusMotherboard.java
@@ -1,4 +1,17 @@
 package com.cydeo.stereotype_annotation.motherboardfactory;
 
-public class AsusMotherboard {
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class AsusMotherboard extends Motherboard {
+
+
+    public AsusMotherboard(String model, String manufacturer, int ramSlot, int cardSlot, String bios) {
+        super(model, manufacturer, ramSlot, cardSlot, bios);
+    }
+
+    @Override
+    public void loadProgram(String programName) {
+        System.out.println("Program"+ programName+ "is now loading....");
+
+    }
 }
