37a16d10ae867c701aa42d96689df40973266bc4
Kadifa <Kudickadifa@gmail.com>
2022-09-12 09:30:21 -0500

    stereotype annotation

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/stereotype_annotation/casefactory/Case.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/stereotype_annotation/casefactory/Case.java
@@ -1,4 +1,20 @@
 package com.cydeo.stereotype_annotation.casefactory;
 
-public class Case {
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public abstract class Case {
+    private String model;
+    private String manufacturer;
+    private String powerSupply;
+
+    public Case(String model, String manufacturer, String powerSupply) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.powerSupply = powerSupply;
+    }
+
+public abstract void pressPowerButton();
 }
