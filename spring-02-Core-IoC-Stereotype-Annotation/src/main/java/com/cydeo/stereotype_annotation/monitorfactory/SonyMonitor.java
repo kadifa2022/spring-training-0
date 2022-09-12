62af4889b17fa38c23ef9afd74660f016994b1c8
Kadifa <Kudickadifa@gmail.com>
2022-09-12 09:30:56 -0500

    stereotype annotation

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/stereotype_annotation/monitorfactory/SonyMonitor.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/stereotype_annotation/monitorfactory/SonyMonitor.java
@@ -1,4 +1,14 @@
 package com.cydeo.stereotype_annotation.monitorfactory;
 
-public class SonyMonitor {
+public class SonyMonitor extends Monitor {
+
+    public SonyMonitor(String model, String manufacturer, int size) {
+        super(model, manufacturer, size);
+    }
+
+    @Override
+    public void drawPixelAt() {
+        System.out.println("Drawing pixel with Sony");
+
+    }
 }
