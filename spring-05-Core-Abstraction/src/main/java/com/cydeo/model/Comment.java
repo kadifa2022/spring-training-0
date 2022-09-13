ba416d2c510eb8fdcc2c147337d7de6c612652a2
Kadifa <Kudickadifa@gmail.com>
2022-09-13 15:28:21 -0500

    core abstraction

--- spring-05-Core-Abstraction/src/main/java/com/cydeo/model/Comment.java
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/model/Comment.java
@@ -3,7 +3,7 @@ package com.cydeo.model;
 import lombok.Data;
 
 @Data
-public class Comment {
+public class Comment {//never put component in model class no relationship
     private String author;
     private String text;
 }
