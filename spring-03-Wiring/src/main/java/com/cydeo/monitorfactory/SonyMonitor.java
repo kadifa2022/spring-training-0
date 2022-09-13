91b73b155fab60927fdb97f22921863ef90bb1de
Kadifa <Kudickadifa@gmail.com>
2022-09-12 19:20:01 -0500

    wiring

--- spring-03-Wiring/src/main/java/com/cydeo/monitorfactory/SonyMonitor.java
+++ spring-03-Wiring/src/main/java/com/cydeo/monitorfactory/SonyMonitor.java
@@ -1,4 +1,19 @@
 package com.cydeo.monitorfactory;
 
-public class SonyMonitor {
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
+public class SonyMonitor extends Monitor{
+    public SonyMonitor(String model, String manufacturer, int size) {
+        super(model, manufacturer, size);
+    }
+
+    @Override
+    public void drawPixelAt() {
+        System.out.println("Drawing pixel with Sony");
+
+    }
 }
