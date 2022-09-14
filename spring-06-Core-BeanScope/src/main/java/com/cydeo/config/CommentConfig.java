75326a0b805e5c9f67b02ed4decf30fd958ff9fb
Kadifa <Kudickadifa@gmail.com>
2022-09-14 14:55:04 -0500

    core abstraction

---
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/config/CommentConfig.java
@@ -0,0 +1,28 @@
+package com.cydeo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan(basePackages= "com.cydeo")//this is container
+//@ComponentScan(basePackages={"com.cydeo.proxy", "com .cydeo.service", "com.cydeo.repository})
+public class CommentConfig {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
