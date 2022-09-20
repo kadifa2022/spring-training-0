f8b2996e07a088153c4876df9f6c26afd23baf5b
Kadifa <Kudickadifa@gmail.com>
2022-09-20 09:05:01 -0500

    mvc

--- spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -12,10 +12,21 @@ public class HomeController {//create class with controller
     public String getHomePage(){         //which en point will work www.@RequestMapping
         return  "home.html";        //view html
 
-
     }
-    @RequestMapping({"/apple","/orange"})
+    @RequestMapping("/ozzy")
     public String getHomePage2(){
+        return "home.html";
+    }
+
+    @RequestMapping // nothing and /
+    public String getHomePage3(){
+        return "home.html";
+    }
+
+
+
+    @RequestMapping({"/apple","/orange"})
+    public String getHomePage4(){
         return"home.html";
     }
 
