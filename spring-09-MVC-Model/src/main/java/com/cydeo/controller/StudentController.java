f9afc89095f2823f5684b54e65d3f8cd3549de28
Kadifa <Kudickadifa@gmail.com>
2022-09-20 09:05:12 -0500

    mvc

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StudentController {
-    @RequestMapping("/welcome")//TYMELEAF tamplate
-    public String homePage(Model model){//methode mode to take data to view
-        model.addAttribute("name", "Cydeo");
-        model.addAttribute("course", "MVC");
-        return "student/welcome";
+    @RequestMapping("/welcome")
 
+    public String homePage(Model model){
+
+
+        model.addAttribute("name","Cydeo");
+        model.addAttribute("course","MVC");
+
+        return "student/welcome";
     }
 }
