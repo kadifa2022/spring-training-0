8f79893dfb743b1ae33e7caf41d3cd92199b7113
Kadifa <Kudickadifa@gmail.com>
2022-09-26 22:20:58 -0500

    thymeleaf class

--- spring-10-Thymeleaf/src/main/java/com/cydeo/model/Mentor.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/model/Mentor.java
@@ -2,13 +2,19 @@ package com.cydeo.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Mentor {
 
       private String firstName;
         private String lastName;
-        private int age;
-        private String state;
+        private String email;
+        private String gender;
+        private String batch;
+        private boolean graduate;
+        private String company;
+
 }
