f0673222037e8a02c87e2ec3d962b0c45276afef
Kadifa <Kudickadifa@gmail.com>
2022-09-26 22:21:14 -0500

    thymeleaf class

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,25 +1,49 @@
 package com.cydeo.controller;
 
-import com.cydeo.bootstrap.DataGenerator;
+
+import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
     //localhost:8080/mentor/register
-    @RequestMapping("register")
-    public String register(Model model){
-       model.addAttribute("mentors", DataGenerator.createStudent());
+     @GetMapping("/register")
+        public String register(Model model){
 
-        return"mentors/register";
+            List<String> batchList = Arrays.asList("JD1","JD2","JD3");//created for dropDown
+            model.addAttribute("batchList",batchList);
+
+            model.addAttribute("mentor",new Mentor());
+
+            return "mentor/mentor-register";
+        }
+
+        @PostMapping("/confirm")
+        public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+
+
+//        return "mentor/mentor-register";
+            return "redirect:/mentor/register";
+        }
+
+
+/*
+    @GetMapping("/confirm")
+    public String submitForm2() {
+        return "mentor/mentor-confirmation";
+
+
     }
-
-    @RequestMapping("/drop")//localhost:8080/mentor/drop
-    public String drop(){
-        return "student/register";
-    }
-
+    */
 }
