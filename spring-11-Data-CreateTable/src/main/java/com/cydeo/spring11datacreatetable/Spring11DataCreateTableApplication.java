ead1c20ac22afe20efe7844260039ec05888c501
Kadifa <Kudickadifa@gmail.com>
2022-10-20 20:09:38 -0500

    table creation

---
+++ spring-11-Data-CreateTable/src/main/java/com/cydeo/spring11datacreatetable/Spring11DataCreateTableApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring11datacreatetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring11DataCreateTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring11DataCreateTableApplication.class, args);
+	}
+
+}
