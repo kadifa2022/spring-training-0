f531f82580e24108ec7eb374e11de3b275173744
Kadifa <Kudickadifa@gmail.com>
2022-10-20 20:09:27 -0500

    table creation

---
+++ spring-11-Data-CreateTable/src/test/java/com/cydeo/spring11datacreatetable/Spring11DataCreateTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring11datacreatetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring11DataCreateTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
