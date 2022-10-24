4a33591d65c0c59ee0f731346a9d401d00f4c051
Kadifa <Kudickadifa@gmail.com>
2022-10-23 21:02:21 -0500

    table creation practice

--- spring-11-Data-CreateTable/src/main/resources/data.sql
+++ spring-11-Data-CreateTable/src/main/resources/data.sql
@@ -1,3 +1,9 @@
 INSERT INTO EMPLOYEES(id, name)
     VALUES (1, 'Mike');
 
+INSERT INTO students(first_name, last_name, email)
+VALUES('Mike','Smith','mike@cydeo.com');
+
+INSERT INTO students( first_name, last_name, email)
+VALUES('Tom','Hanks','tom@cydeo.com');
+
