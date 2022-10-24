0f7fe53a4b0ff12797c00780d16e4cf1df26ece3
Kadifa <Kudickadifa@gmail.com>
2022-10-23 21:33:17 -0500

    table creation practice

--- spring-11-Data-CreateTable/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-CreateTable/src/main/java/com/cydeo/entity/Student.java
@@ -1,5 +1,7 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.Gender;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,15 +12,28 @@ public class Student {
     @Id
    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long Id;
+
     @Column(name="studentFirstName")
     private String firstName;
     @Column(name="studentLastName")
     private String lastName;
+
     private String email;
+
     @Column(columnDefinition = "DATE")
     private LocalDate birthday;
     @Column(columnDefinition = "DATE")
     private LocalDate birthTime;
+
     @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime birthDayTime;
+
+    @Enumerated(EnumType.STRING)
+   // @Enumerated(EnumType.ORDINAL) creating integer
+    private Gender gender;
+
+    @Transient//for don't show in table will be invisible
+    private String city;
+
+
 }
