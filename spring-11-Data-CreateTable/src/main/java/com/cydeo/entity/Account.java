98e0823d9bb91d3e8dc7056ec2aa939fb7a95532
Kadifa <Kudickadifa@gmail.com>
2022-10-23 21:49:49 -0500

    table creation practice

--- spring-11-Data-CreateTable/src/main/java/com/cydeo/entity/Account.java
+++ spring-11-Data-CreateTable/src/main/java/com/cydeo/entity/Account.java
@@ -1,8 +1,11 @@
 package com.cydeo.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
-
+@MappedSuperclass
 public class Account {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY) //postgres is assigning id for us
     private String id;
     private String Owner;
     private BigDecimal balance;
