be4e2f90cf14f6e291d71a3f4de988cd7bb5eecb
Kadifa <Kudickadifa@gmail.com>
2022-10-23 21:01:44 -0500

    table creation practice

---
+++ spring-11-Data-CreateTable/src/main/java/com/cydeo/entity/Account.java
@@ -0,0 +1,10 @@
+package com.cydeo.entity;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private String id;
+    private String Owner;
+    private BigDecimal balance;
+    private BigDecimal interestRate;
+}
