a0e0512466d1ba480d40ab15b7b39288906adb22
Kadifa <Kudickadifa@gmail.com>
2022-10-24 14:36:25 -0500

    table data relationship

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Cart.java
@@ -0,0 +1,20 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
