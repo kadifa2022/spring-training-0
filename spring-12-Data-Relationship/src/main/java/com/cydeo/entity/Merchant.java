5d41cd47efacec9a001331a54dd6aecd895826f4
Kadifa <Kudickadifa@gmail.com>
2022-10-24 14:13:32 -0500

    table data relationship

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Merchant.java
@@ -0,0 +1,35 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name="merchants")
+public class Merchant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+    private BigDecimal transactionFee;
+    private  BigDecimal commissionRate;
+    private Integer payoutDelayCount;
+    @OneToMany(mappedBy = "merchant")//in oneToMany relationship belong to
+    private List<Payment> paymentList;       //for relationship still complaining-more than one object(add list of payments)
+
+    public Merchant(String name, String code, BigDecimal transactionFee, BigDecimal commissionRate, Integer payoutDelayCount) {
+        this.name = name;
+        this.code = code;
+        this.transactionFee = transactionFee;
+        this.commissionRate = commissionRate;
+        this.payoutDelayCount = payoutDelayCount;
+    }
+}
+
