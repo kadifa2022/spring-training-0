7040e92f7229f175565c2e8f61a9f25202220287
Kadifa <Kudickadifa@gmail.com>
2022-10-25 12:22:24 -0500

    table data relationship

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/repository/CostumerRepository.java
@@ -0,0 +1,8 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Costumer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CostumerRepository extends JpaRepository<Costumer, Long> {
+
+}
