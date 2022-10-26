8967ee031e0aa1b7d633fac79707347c02624849
Kadifa <Kudickadifa@gmail.com>
2022-10-26 07:58:56 -0500

    cinema lab practice

---
+++ spring-13-Data-CinemaLab-Practice/src/main/java/com/cydeo/Entity/Ticket.java
@@ -0,0 +1,25 @@
+package com.cydeo.Entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Ticket extends BaseEntity{
+@Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime dateTime;
+    private Integer seatNumber;
+    private Integer rowNumber;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    private MovieCinema movieCinema;
+    @ManyToOne(fetch= FetchType.LAZY)
+    private User userAccount;
+}
