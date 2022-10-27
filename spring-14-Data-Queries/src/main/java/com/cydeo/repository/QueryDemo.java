1f8b976f9f53c09689c1d4c33bc2518fbc5f13aa
Kadifa <Kudickadifa@gmail.com>
2022-10-27 18:34:02 -0500

    data queries

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/QueryDemo.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/QueryDemo.java
@@ -1,23 +1,32 @@
 package com.cydeo.repository;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component//runner class
 public class QueryDemo implements CommandLineRunner {
-    private final RegionRepository regionRepository;
-    private final DepartmentRepository departmentRepository;
 
-    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
+
+    private final RegionRepository regionRepository;
+
+    public QueryDemo(RegionRepository regionRepository) {
         this.regionRepository = regionRepository;
-        this.departmentRepository = departmentRepository;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
+                                    //using squel with methods that we created
         System.out.println("findByCountry: " + regionRepository.findByCountry("Canada"));
-        System.out.println("findByCountryContaining: " + regionRepository.findByCountryContaining("United"));
-        System.out.println("findByCountryContainsOrderByRegionDesc:"+ regionRepository.findByCountryContainsOrderByRegionDesc("United"));
+       /*
+      System.out.println("findByCountryContaining: " + regionRepository.findByCountryContaining("United"));
+        System.out.println("findByCountryContainsOrderByRegion:"+ regionRepository.findByCountryContainsOrderByRegion("United"));
 
         System.out.println("Departments----");
+
+        System.out.println("findByDivision:"+departmentRepository.findDistinctByTop3ByDivisionContains("Hea"));
+    }
+
+        */
     }
 }
