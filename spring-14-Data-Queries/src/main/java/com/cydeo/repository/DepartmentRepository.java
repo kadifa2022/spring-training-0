4f663b53b652d91a0e55ab9ddbbadcc4948caf15
Kadifa <Kudickadifa@gmail.com>
2022-10-27 19:25:54 -0500

    data queries

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/DepartmentRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/DepartmentRepository.java
@@ -7,19 +7,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DepartmentRepository extends JpaRepository<Department, String> {
-/*
+
     //Display all departments in the furniture department
    List<Department> findByDepartment(String Department);
    //Display all departments in Health Division
-    List<Department>findByDivision(String division);
-    List <Department>findByDivisionIs(String division);
-    List<Department>findByDivisionEquals(String division);
+    List<Department> findByDivision(String division);
+    List <Department>findByDivisionIs(String division);//same as findByDivision
+    List<Department>findByDivisionEquals(String division);//same as findByDivision
     //Display all departments wth division name ends with 'ics'
     List<Department>findByDivisionEndsWith(String patter);
+
     //display top 3 departments with division name includes 'Hea' without duplicates
+    List<Department> findDistinctTop3ByDivisionContains(String patter);
 
-    List<Department>findDistinctByTop3ByDivisionContains(String pattern);
 
-*/
+
+
 
 }
