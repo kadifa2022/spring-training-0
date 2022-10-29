2bdc8fe33b7ee83d1876962341248d3087235197
Kadifa <Kudickadifa@gmail.com>
2022-10-28 19:49:30 -0500

    data cinema lab

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -2,11 +2,13 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-
+//JPQL
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     //Display all employees with email address
@@ -31,7 +33,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     List<Employee> findBySalaryLessThan(Integer salary);
 
     //Display all employees that has been hired between "" and ""
-    List<Employee> findByHireDateBetween(LocalDate startDate, LocalDate endDate);
+//    List<Employee> findByHireDateBetween(LocalDate startDate, LocalDate endDate);
 
     //Display all employees where salaries greater and equal to "" in order-salary
     List<Employee> findBySalaryGreaterThanEqualOrderBySalary(Integer salary);
@@ -43,6 +45,55 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //Display all employees that do not have email address
 
     List<Employee> findByEmailIsNull();
+
+
+        //JPQL -Everything is related to Entity
+        @Query("SELECT employee FROM Employee employee WHERE employee.email='acurwood6@1und1.de' ")
+        Employee retrieveEmployeeDetails();
+        @Query("SELECT e.salary FROM Employee e WHERE e.email='acurwood6@1und1.de'")
+       Integer retrieveEmployeeSalary();
+
+        //NOT EQUaEL
+    @Query("SELECT e FROM Employee e WHERE e.salary<>?1")//not equal <>
+    List<Employee> retrieveEmployeeSalaryNotEqual(int salary);
+
+    //Like / contains /startswiths / endswith
+    @Query("SELECT e FROM Employee e WHERE e.firstName LIKE ?1")
+    List<Employee>retrieveEmployeeFirstNameLike(String pattern);
+
+    //Less than
+    @Query("SELECT e.firstName FROM Employee  e WHERE e.salary< ?1")
+    List<String>retrieveEmployeeSalaryLessThan(int salary);
+
+    @Query("SELECT e FROM Employee  e WHERE e.salary>?1")
+    List<Employee>retrieveEmployeeSalaryGreaterThan(int salary);
+   // BETWEEN
+
+    @Query("SELECT e FROM Employee  e WHERE e.salary BETWEEN ?1 AND ?2")
+    List<Employee>retrieveEmployeeSalaryBetween(int salary1, int salary2);
+    //BEFORE
+    @Query("SELECT e FROM Employee  e WHERE e.hireDate >?1")
+    List<Employee> retrieveEmployeeHireDateBefore(LocalDate date);
+
+    //NULL
+    @Query("SELECT e FROM Employee  e WHERE e.email IS NULL ")
+    List<Employee> retrieveEmployeeEmailIsNull();
+
+    @Query("SELECT e FROM Employee  e WHERE e.email IS NOT NULL ")
+    List<Employee> retrieveEmployeeEmailIsNotNull();
+    //SortinG in asc  AND DESC order
+    @Query("SELECT e FROM Employee e ORDER BY e.salary")
+    List<Employee> retrieveEmployeeSalaryOrderAsc();
+    @Query("SELECT e FROM Employee e ORDER BY e.salary DESC")
+    List<Employee> retrieveEmployeeSalaryOrderAscDesc();
+
+    //Native Query  first write @query (value pure sql query) and native query
+    @Query(value="SELECT * FROM Employee   WHERE  salary = ?1", nativeQuery = true)
+    List<Employee> retrieveEmployeeDetailBySalary(int salary);
+
+    //Named Parameter--based on name positional
+    @Query("SELECT e FROM Employee e WHERE e.salary=:salary")
+    List<Employee> retrieveEmployeeSalary(@Param("salary") int salary);
 
 
 
