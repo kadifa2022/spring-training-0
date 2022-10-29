381f795eabf82f7e8b143720c20a910ad7eaac7c
Kadifa <Kudickadifa@gmail.com>
2022-10-28 19:50:22 -0500

    data cinema lab

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/QueryDemo.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/QueryDemo.java
@@ -9,10 +9,12 @@ public class QueryDemo implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
     private final DepartmentRepository departmentRepository;
+    private final EmployeeRepository employeeRepository;
 
-    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
+    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
         this.regionRepository = regionRepository;
         this.departmentRepository = departmentRepository;
+        this.employeeRepository = employeeRepository;
     }
 
 
@@ -39,6 +41,22 @@ public class QueryDemo implements CommandLineRunner {
         System.out.println("findDistinctTop3ByDivisionContains:"+ departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
 
         System.out.println("----------------------------Employees----------------------------------");
+
+        System.out.println("findAllByFirstNameAndLastNameOrEmail:" +employeeRepository.findAllByFirstNameAndLastNameOrEmail("Jodi","Hook", "jhookd@booking.com"));
+        System.out.println("findByEmail:" + employeeRepository.findByEmail("ssymonds2@hhs.gov"));
+      //  System.out.println("findByFirstNameIsNot" + employeeRepository.findByFirstNameIsNot("''"));
+        System.out.println("findByLastNameStartsWith:" + employeeRepository.findByLastNameStartsWith("Ja"));
+        System.out.println("findBySalaryGreaterThan:" + employeeRepository.findBySalaryGreaterThan(150000));
+        System.out.println("findBySalaryLessThan:" + employeeRepository.findBySalaryLessThan(60000));
+      //  System.out.println("findByHireDateBetween:" + employeeRepository.findByHireDateBetween(2006-04-20,2011-11-13);
+        System.out.println("findBySalaryGreaterThanEqualOrderBySalary:"+ employeeRepository.findBySalaryGreaterThanEqualOrderBySalary(12000));
+        System.out.println("findDistinctTop3BySalaryLessThan:" + employeeRepository.findDistinctTop3BySalaryLessThan(90000));
+        System.out.println("findByEmailIsNull:" + employeeRepository.findByEmailIsNull());
+
+
+        //- JPQL Everything is related to entity
+        System.out.println(employeeRepository.retrieveEmployeeDetails());
+        System.out.println(employeeRepository.retrieveEmployeeSalary());
 
 
 
