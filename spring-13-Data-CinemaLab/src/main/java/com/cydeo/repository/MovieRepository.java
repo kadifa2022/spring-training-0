4f7f4c7959d640e53124cc3f3684a24b8a5582c4
Kadifa <Kudickadifa@gmail.com>
2022-10-31 13:54:57 -0500

    movie cinema repository queries

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/MovieRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/MovieRepository.java
@@ -1,27 +1,34 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Movie;
+import com.cydeo.enums.MovieState;
+import com.cydeo.enums.MovieType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     // ------------------- DERIVED QUERIES ------------------- //
 
-    //Write a derived query to read a movie with a name
-
+    //Write a derived query to read a movie with a name //one movie
+    Optional<Movie> findByName(String name);
 
     //Write a derived query to list all movies between a range of prices
-
-
+    List<Movie> findAllByPriceBetween(BigDecimal price1, BigDecimal price2);
+     //LIST of something
     //Write a derived query to list all movies where duration exists in the specific list of duration
-
+    List<Movie>findAllByDurationIn(List<Integer> durations);
 
     //Write a derived query to list all movies with higher than a specific release date
-
-
+    List<Movie> findAllByReleaseDateAfter(LocalDate releaseDate);
     //Write a derived query to list all movies with a specific state and type
+    List<Movie> findAllByStateAndType(MovieState state, MovieType type );
 
 
     // ------------------- JPQL QUERIES ------------------- //
