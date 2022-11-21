c833bd4c0e99cc9a774c6b3ab49a2054b11ba67f
Kadifa <Kudickadifa@gmail.com>
2022-11-21 07:20:34 -0600

    query practice

---
+++ spring-13-Data-CinemaLab-Practice/src/main/java/com/cydeo/repository/GenreRepository.java
@@ -0,0 +1,24 @@
+package com.cydeo.repository;
+
+import com.cydeo.Entity.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+
+    //-----------------------JPQL-QUERIES-----------------------//
+
+    //Write JPQL query that return all genres
+    @Query("SELECT g FROM Genre g")
+    List<Genre> fetchAll();
+
+
+    //Write a native query that returns genres by containing name
+    @Query(value = "SELECT * FROM Genre WHERE  name ILIKE concat('%',?1,'%')", nativeQuery = true)
+    List<Genre> retrieveByName(@Param("name") String name);
+}
