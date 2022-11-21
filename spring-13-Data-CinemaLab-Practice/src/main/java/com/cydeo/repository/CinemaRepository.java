6e28756b2ba0c8200a313bc6c182c256deb8ddbb
Kadifa <Kudickadifa@gmail.com>
2022-11-20 21:24:48 -0600

    query practice

--- spring-13-Data-CinemaLab-Practice/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ spring-13-Data-CinemaLab-Practice/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -2,6 +2,25 @@ package com.cydeo.repository;
 
 import com.cydeo.Entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
+
+    //---------------------DERIVED QUERIES------------------------//
+
+    //Write derived query to get cinema with specific name
+    Optional<Cinema>findByName(String name);//optional because of throwing exception
+
+    //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
+    List<Cinema> findFirst3bySponsoredNameContainingOrderBySponsoredName(String sponsoredName);
+    //Write a derived query to list all cinemas in specific country
+
+    List<Cinema> findAllByLocationCountry(String country);
+    //Write a derived query to list all cinemas with a specific name or sponsored name
+    List<Cinema> findAllNameOrBySponsoredName(String name, String sponsoredName);
+
 }
