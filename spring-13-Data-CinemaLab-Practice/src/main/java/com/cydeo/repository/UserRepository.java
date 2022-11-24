142158623ee934259eab563b53b8a77f3fb4bcb9
Kadifa <Kudickadifa@gmail.com>
2022-11-24 09:25:40 -0600

    query practice

--- spring-13-Data-CinemaLab-Practice/src/main/java/com/cydeo/repository/UserRepository.java
+++ spring-13-Data-CinemaLab-Practice/src/main/java/com/cydeo/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.cydeo.repository;
 
 import com.cydeo.Entity.User;
+import org.hibernate.cache.spi.SecondLevelCacheLogger;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -32,6 +33,35 @@ public interface UserRepository extends JpaRepository<User, Long> {
     //Write a JPQL query than returns a user read by email?
     @Query("SELECT u FROM User u WHERE u.email=?1")
     Optional<User> fetchByEmailJPQL(@Param("email")String email);
+
+    //Write a JPQL query that returns a user read by username?
+    @Query("SELECT u FROM User u WHERE u.username=?1")
+    Optional<User> fetchByUsernameJPQL(@Param("username")String username);
+
+    //Write a JPQL query that returns all users
+    @Query("SELECT u FROM User  u")
+    List<User> fetchAllUsers();
+
+    //---------------------NATIVE QUERIES------------------------------//
+    //Write native query that  returns all users that contain a specific name?
+
+    @Query(value = "SELECT * FROM user_account u " +
+            "JOIN account_details ad ON ad.id=u.account_details_id WHERE ad.name ILIKE concat('%', ?1, '%')", nativeQuery = true)
+        List<User> retrieveAllByName(@Param("name") String name);
+
+    //Write a native query that returns all users
+    @Query(value = "SELECT * USER user_account", nativeQuery = true)
+    List<User> retrieveAll();
+
+    //Write a native query that returns all users in the range of ages?
+    @Query(value = "SELECT * FROM user_account u " +
+            "JOIN account_details ad ON ad.id=u.account_details_id WHERE ad.age BETWEEN ?1 AND ?2 ", nativeQuery = true)
+    List<User> retrieveBetweenAgeRange();
+
+  //write a native query to read a user by email?
+    @Query(value = "SELECT * FROM user_account WHERE email=?1", nativeQuery = true)
+    User retrieveByEmail(@Param("email")String email);
+
 
 
 
