a5e837e57836627b35287f5577f8be3d8b3c13bc
Kadifa <Kudickadifa@gmail.com>
2022-11-27 14:01:52 -0600

    rest app

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -2,11 +2,10 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
+import jdk.jfr.Category;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,13 +17,24 @@ public class CourseController_ResponseEntity {
     public CourseController_ResponseEntity(CourseService courseService) {
         this.courseService = courseService;
     }
-    @GetMapping
+    @GetMapping    //get the list
     public ResponseEntity<List<CourseDTO>> getAllCourses(){//generic changing status code 200,...//modifying output
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .header("Version", "Cydeo.v2")
                 .header("Operation", "Get List")
                 .body(courseService.getCourses());//list -ResponseEntity
+    }
+    //get one
+    @GetMapping({"{id}"})
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id")long courseId){
+        return  ResponseEntity
+                .ok(courseService.getCourseById(courseId)); //ok.200
 
     }
+    @GetMapping("category/{name}")
+    public ResponseEntity<List<CourseDTO>> getCourseCategory(@PathVariable("name") String category){
+        return ResponseEntity.ok(courseService.getCoursesByCategory(category));
+    }
+
 }
