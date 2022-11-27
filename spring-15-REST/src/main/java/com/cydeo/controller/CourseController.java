785d42b322e4a1b1361e3ba95ed94442d983d519
Kadifa <Kudickadifa@gmail.com>
2022-11-27 13:50:51 -0600

    rest app

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/courses/api/v1")//way
 public class CourseController {
     private final CourseService courseService;
 
