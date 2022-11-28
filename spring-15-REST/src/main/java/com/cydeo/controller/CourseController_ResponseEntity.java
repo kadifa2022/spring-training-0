24741070c25c66a4e694d1d736b5dafa6266a1bc
Kadifa <Kudickadifa@gmail.com>
2022-11-28 12:15:16 -0600

    rest app

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -17,32 +17,45 @@ public class CourseController_ResponseEntity {
     public CourseController_ResponseEntity(CourseService courseService) {
         this.courseService = courseService;
     }
+
     @GetMapping    //get the list
-    public ResponseEntity<List<CourseDTO>> getAllCourses(){//generic changing status code 200,...//modifying output
+    public ResponseEntity<List<CourseDTO>> getAllCourses() {//generic changing status code 200,...//modifying output
         return ResponseEntity//modifying output
                 .status(HttpStatus.ACCEPTED)
                 .header("Version", "Cydeo.V2")
                 .header("Operation", "Get List")
                 .body(courseService.getCourses());//list -ResponseEntity
     }
+
     //get one
     @GetMapping("{id}")
-    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id")long courseId){
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id") long courseId) {
         return ResponseEntity
                 .ok(courseService.getCourseById(courseId)); //ok.200
 
     }
+
     @GetMapping("category/{name}")
-    public ResponseEntity<List<CourseDTO>> getCourseCategory(@PathVariable("name") String category){
+    public ResponseEntity<List<CourseDTO>> getCourseCategory(@PathVariable("name") String category) {
         return ResponseEntity.ok(courseService.getCoursesByCategory(category));
     }
+
     @PostMapping
-    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO course){
+    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO course) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .header("Operation", "Create")
                 .body(courseService.createCourse(course));
 
     }
-
+@DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteCourseById(@PathVariable("id") Long courseId) {
+        courseService.deleteCourseById(courseId);
+        return ResponseEntity.noContent().build();
+    }
+@PutMapping("{id}")
+    public ResponseEntity<Void> updateCourse(@PathVariable("id") Long courseId, @RequestBody CourseDTO course){
+       courseService.updateCourse(courseId,course);
+      return ResponseEntity.noContent().build();
+}
 }
