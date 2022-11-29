e83b512ce4a754a7b3d91a78507fa8e0896fcfa8
Kadifa <Kudickadifa@gmail.com>
2022-11-28 20:45:59 -0600

    rest app

--- spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
@@ -1,4 +1,4 @@
-package com.cydeo;
+package com.cydeo.controller;
 
 import com.cydeo.dto.User;
 import org.springframework.http.*;
@@ -17,26 +17,26 @@ public class Consume_RestTemplate {
 
     private final String URI = "https://jsonplaceholder.typicode.com/users";
 
-    private final RestTemplate restTemplate;//
+    private final RestTemplate restTemplate;//DI To access
 
     public Consume_RestTemplate(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @GetMapping
-    public ResponseEntity<User[]>readAllUsers(){
+    public ResponseEntity<User[]>readAllUsers(){//Without DTO CLASS
        return  restTemplate.getForEntity(URI, User[].class);//accepting []and output will be User[] and returning baseEntity
 
     }
     @GetMapping("{id}")
     public Object readUser(@PathVariable("id") Integer id){
-        String URL = URI + "/{id}";//how we gonna hit the end point
-        return restTemplate.getForObject(URL, Object.class,id);//give me url i give you whole class
+        String URL = URI + "/{id}";//how we gona hit the end point
+        return restTemplate.getForObject(URL, Object.class,id);//give me url i give you wholeDTO class
 
     }
     @GetMapping("/test")
     public ResponseEntity<Object> consumePostFromDummyApi(){
-        HttpHeaders headers = new HttpHeaders();
+        HttpHeaders headers = new HttpHeaders();//Passing headers
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         headers.set("app-id","6298ebfecd0551211fce37a6");
 
