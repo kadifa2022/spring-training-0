a40bdb0a34aab302a2baa8c4e4f165faa3be6f6c
Kadifa <Kudickadifa@gmail.com>
2022-11-29 11:02:49 -0600

    school lab

--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
@@ -1,12 +1,19 @@
 package com.cydeo.dto;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//@JsonIgnoreProperties(value = {"address", "country","state"}, ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AccountDTO {
 
+   @JsonIgnore
     private String name;
     private String address;
     private String country;
@@ -14,6 +21,8 @@ public class AccountDTO {
     private String city;
     private Integer age;
     private String postalCode;
-    private UserDTO user;
+
+  @JsonBackReference//this field not serialized
+  private UserDTO user;
 
 }
