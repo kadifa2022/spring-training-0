6042ccc077cfc90c55da91ac5badb104c858e02a
Kadifa <Kudickadifa@gmail.com>
2022-12-02 01:17:34 -0600

    spring 19 REST OpenAPI3

--- spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
+++ spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
@@ -3,6 +3,10 @@ package com.cydeo.controller;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,12 +27,23 @@ public class UserController {
 
     @GetMapping("/list")
     @Operation(summary = "Read all users" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved users (OK)",
+            content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+    })
+
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.getUsers());
     }
 
     @PostMapping
     @Operation(summary = "Create a user")
+    @ApiResponse(responseCode = "201", description = "User crated successfully (CREATED)",
+                content={@Content(mediaType = "application/hml"), @Content(mediaType = "application/json")},
+    headers = @Header(name = "Connection", description = "keep-alive"))
+
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(userDTO));
     }
