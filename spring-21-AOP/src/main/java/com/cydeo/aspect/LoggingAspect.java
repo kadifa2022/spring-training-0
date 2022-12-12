5dfbc3360d6028cb937c71268aea99b79e5637f4
Kadifa <Kudickadifa@gmail.com>
2022-12-11 19:16:14 -0600

    spring 21 AOP Logging aspect custom annotation

--- spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.cydeo.aspect;
 
 import com.cydeo.dto.CourseDTO;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -94,11 +95,58 @@ public class LoggingAspect {
     //CourseDTO->Object --> this is OK
     //List<CourseDTO> -> List<Object> --> this is not OK
 
-    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
-    public void afterReturningGetMappingAnnotation(){}
-    @AfterThrowing(pointcut = "afterReturningGetMappingAnnotation()", throwing = "exception")
-    public void afterThrowingGetMappingOperation(JoinPoint joinPoint,RuntimeException exception){
-        logger.error("After Throwing  -> Method: {}, Exception: {}"
-        , joinPoint.getSignature().toShortString(), exception.getMessage());
+//    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+//    public void afterReturningGetMappingAnnotation(){}
+//    @AfterThrowing(pointcut = "afterReturningGetMappingAnnotation()", throwing = "exception")
+//    public void afterThrowingGetMappingOperation(JoinPoint joinPoint,RuntimeException exception){
+//        logger.error("After Throwing  -> Method: {}, Exception: {}"
+//        , joinPoint.getSignature().toShortString(), exception.getMessage());
+//    }
+
+
+    @Pointcut("@annotation(com.cydeo.annotation.LoggingAnnotation)")
+    public void loggingAnnotationPC(){}
+
+    @Around ("loggingAnnotationPC()")
+    public Object anyLoggingAnnotationOperation(ProceedingJoinPoint proceedingJoinPoint){
+        logger.info("Before -> Method: {} - Parameter {}"
+        , proceedingJoinPoint.getSignature().toShortString(), proceedingJoinPoint.getSignature());
+
+        Object result = null;
+        try{
+            result = proceedingJoinPoint.proceed();
+;        }catch (Throwable throwable){
+            throwable.printStackTrace();
+        }
+
+        logger.info("After -. Method: {} -Result: {}"
+        , proceedingJoinPoint.getSignature().toShortString(), result.toString());
+        return result;
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
