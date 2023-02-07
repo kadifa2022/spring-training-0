ff0d81b7979f7807eaac1dad5ceaba665c590f47
Kadifa <Kudickadifa@gmail.com>
2023-02-07 16:41:13 -0600

    abstraction practice

--- spring-05-Core-AbstractionPractice/src/main/java/com/cydeo/service/CommentService.java
+++ spring-05-Core-AbstractionPractice/src/main/java/com/cydeo/service/CommentService.java
@@ -3,12 +3,18 @@ package com.cydeo.service;
 import com.cydeo.entity.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class CommentService {
 
-     private CommentRepository commentRepository; //we are  putting interfaces because of loosely couple
-     private CommentNotificationProxy commentNotificationProxy;
+     private final CommentRepository commentRepository; //we are  putting interfaces because of loosely couple
+     private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
 
     public void publishComment(Comment comment) {
 
