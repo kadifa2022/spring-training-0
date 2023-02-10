4635f3bc7ec0a338969180dbdffe5d16b0203509
Kadifa <Kudickadifa@gmail.com>
2023-02-09 20:49:54 -0600

    abstraction practice

---
+++ spring-09-MVC-Model/src/main/java/com/cydeo/model/Student2.java
@@ -0,0 +1,14 @@
+package com.cydeo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student2 {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String color;
+}
