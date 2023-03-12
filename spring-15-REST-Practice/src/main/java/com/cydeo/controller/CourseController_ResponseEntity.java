211a5cb4271f563b9feba25fdb7fec732ae389e4
Kadifa <Kudickadifa@gmail.com>
2023-03-12 18:55:36 -0500

    Spring data practice

--- spring-15-REST-Practice/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
+++ spring-15-REST-Practice/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -18,44 +18,32 @@ public class CourseController_ResponseEntity {
         this.courseService = courseService;
     }
 
-    @GetMapping    //get the list
+    @GetMapping    //get the list -responseEntity class is from Spring framework
     public ResponseEntity<List<CourseDTO>> getAllCourses() {//generic changing status code 200->202 custom created...//modifying output
         return ResponseEntity//modifying output
                 .status(HttpStatus.ACCEPTED)
-                .header("Version", "Cydeo.V2")
+                .header("Version", "Cydeo.V2")//we can pass more than one header
                 .header("Operation", "Get List")
-                .body(courseService.getCourses());//list -ResponseEntity
+                .body(courseService.getCourses());//DATA is coming with body returning list -ResponseEntity
     }
 
     //get one
     @GetMapping("{id}")
     public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id") long courseId) {
-        return ResponseEntity
-                .ok(courseService.getCourseById(courseId)); //ok.200
-
+        return ResponseEntity.ok(courseService.getCourseById(courseId));
     }
-
     @GetMapping("category/{name}")
-    public ResponseEntity<List<CourseDTO>> getCourseCategory(@PathVariable("name") String category) {
+    public ResponseEntity<List<CourseDTO>> getCourseByCategory(@PathVariable("name") String category){
         return ResponseEntity.ok(courseService.getCoursesByCategory(category));
     }
-
     @PostMapping
-    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO course) {
+    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO course){//201 created//202 accepted
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .header("Operation", "Create")
                 .body(courseService.createCourse(course));
+    }
+
+
 
     }
-@DeleteMapping("{id}")
-    public ResponseEntity<Void> deleteCourseById(@PathVariable("id") Long courseId) {
-        courseService.deleteCourseById(courseId);
-        return ResponseEntity.noContent().build();
-    }
-@PutMapping("{id}")
-    public ResponseEntity<Void> updateCourse(@PathVariable("id") Long courseId, @RequestBody CourseDTO course){
-       courseService.updateCourse(courseId,course);
-      return ResponseEntity.noContent().build();
-}
-}
