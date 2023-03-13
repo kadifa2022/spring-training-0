2c0ce125a6df64779ecbf97f11031ab95b208a02
Kadifa <Kudickadifa@gmail.com>
2023-03-13 15:15:31 -0500

    Spring data practice

--- spring-15-REST-Practice/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
+++ spring-15-REST-Practice/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
@@ -1,13 +1,12 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.CourseDTO;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/courses/api/v3")
 public class CourseController_ResponseWrapper {//
@@ -32,6 +31,23 @@ public class CourseController_ResponseWrapper {//
     public ResponseEntity<ResponseWrapper> getCourseById(@PathVariable("id") long courseId){
         return ResponseEntity.ok(new ResponseWrapper("course:" + courseId + "retrieved", courseService.getCourseById(courseId)));
     }
+    @GetMapping("category/{name}")
+    public ResponseEntity<ResponseWrapper> getCourseByCategory(@PathVariable("name")String category){
+        return ResponseEntity.ok((new ResponseWrapper("category retrieved" , courseService.getCoursesByCategory(category))));
+    }
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createCourse(@RequestBody CourseDTO course){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Opearation" , "Create")
+                .body(new ResponseWrapper("Course successfully created ", courseService.createCourse(course)));
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<CourseDTO> deleteCourseById(@PathVariable("id") Long courseId){
+        courseService.deleteCourseById(courseId);
+       return ResponseEntity.noContent().build();
+    }
+
 }
 
 
