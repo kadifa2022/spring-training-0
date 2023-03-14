58650c2a24b916afbb1ae26dbbb767f4a8239ea0
Kadifa <Kudickadifa@gmail.com>
2023-03-13 19:07:13 -0500

    Spring data practice

--- spring-17-REST-CconsumingApisPractice/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
+++ spring-17-REST-CconsumingApisPractice/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
@@ -1,25 +1,46 @@
 package com.cydeo.controller;
 
 import com.cydeo.DTO.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+
 @RestController
 @RequestMapping("/cydeo")
 public class Consume_RestTemplate {
-    private final String URI="https://jsonplaceholder.typicode.com/users";
+    private final String URI = "https://jsonplaceholder.typicode.com/users";
     private final RestTemplate restTemplate;
 
     public Consume_RestTemplate(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
-    @GetMapping
-    public ResponseEntity<User[]> readAllUsers(){
-        return restTemplate.getForEntity(URI, User[].class);
 
+    @GetMapping
+    public ResponseEntity<User[]> readAllUsers() {
+        return restTemplate.getForEntity(URI, User[].class);
+    }
+
+    @GetMapping("{id}")
+    public Object readUser(@PathVariable("id") Integer id) {
+
+        String URL = URI + "/id";
+        return restTemplate.getForObject(URL, Object.class, id);
+
+    }
+
+    @GetMapping("/test")         // syntax of creation Http headers//headers to see
+    public ResponseEntity<Object> consumePostFromDummyApi() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","6298ebfecd0551211fce37a6");
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        //for passing header using exchange()
+        return restTemplate.exchange("https://dummyapi.io/data/v1/user?limit=10", HttpMethod.GET, entity, Object.class);
 
     }
 }
